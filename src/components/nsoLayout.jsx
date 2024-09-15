cbbd011f22fda4ae7ea1d279c3257e24ac7a719e
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-09-15 15:14:42 +0530

    hello

--- src/components/nsoLayout.jsx
+++ src/components/nsoLayout.jsx
@@ -11,6 +11,7 @@ export const loader = async () => {
     const data = await res.json();
     console.log('loader running');
     if (!data?.data?.user) return null;
+    console.log(data?.data?.user);
     return data?.data?.user;
 };
 
