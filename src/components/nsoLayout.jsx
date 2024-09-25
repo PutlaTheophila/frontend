6e88810ccbaaf23967e3f618a772557b27aac079
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-09-26 01:40:35 +0530

    hello

--- src/components/nsoLayout.jsx
+++ src/components/nsoLayout.jsx
@@ -4,17 +4,25 @@ import AttendanceNavbar from "./nsocomp/attendanceBar";
 
 
 export const loader = async () => {
-    const res = await fetch("https://terabyte-kvey.onrender.com/api/v1/auth", {
-      method: 'GET',
-      credentials: 'include',
-    });
-    // updated
-    const data = await res.json();
-    console.log('loader running');
-    if (!data?.data?.user) return null;
-    console.log(data?.data?.user);
-    return data?.data?.user;
+    try {
+        const res = await fetch("https://terabyte-kvey.onrender.com/api/v1/auth", {
+            method: 'GET',
+            credentials: 'include',
+        });
+        if (!res.ok) {
+            throw new Error('Failed to fetch user data');
+        }
+        const data = await res.json();
+        console.log('loader running');
+        if (!data?.data?.user) return null;
+        console.log(data?.data?.user);
+        return data?.data?.user;
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        return null;
+    }
 };
+
 
 export default function NsoLayout (){
 
