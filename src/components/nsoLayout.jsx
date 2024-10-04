8f90f8ba95db91102012470b8c11854a11d3db2a
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-10-04 17:18:36 +0530

    hello:

--- src/components/nsoLayout.jsx
+++ src/components/nsoLayout.jsx
@@ -41,7 +41,7 @@ export const loader = async () => {
 
 export default function NsoLayout (){
     const user = useLoaderData();
-    console.log(data);
+    console.log(user);
     return(
         <>
             {/* <NsoNavbar/> */}
