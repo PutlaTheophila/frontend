4de2c179f3230c17a7acebdfdb52a8a413eb7d2a
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-10-04 20:24:14 +0530

    hello

--- src/components/nsoLayout.jsx
+++ src/components/nsoLayout.jsx
@@ -7,7 +7,7 @@ import { GoogleLogin } from '@react-oauth/google';
 
 
 export async function loader() {
-    const res = await fetch("https://terabyte-lvkey.onrender.com/api/v1/tournaments/auth", {
+    const res = await fetch("https://terabyte-kvey.onrender.com/api/v1/auth", {
         method: 'GET',
         credentials: 'include'
     });
