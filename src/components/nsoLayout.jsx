85c7050cc6af3d474957ad7baed164810e8bfc22
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-10-04 20:08:17 +0530

    hello

--- src/components/nsoLayout.jsx
+++ src/components/nsoLayout.jsx
@@ -7,30 +7,14 @@ import { GoogleLogin } from '@react-oauth/google';
 
 
 export async function loader() {
-    try {
-        const res = await fetch("https://terabyte-lvkey.onrender.com/api/v1/auth/details", {
-            method: "GET",
-            credentials: 'include', // Include credentials for cookies or auth
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        });
+    const res = await fetch("https://terabyte-lvkey.onrender.com/api/v1/auth/details", {
+        method: 'GET',
+        credentials: 'include'
+    });
+    const data = await res.json();
+    return data
+}
 
-        // Parse the JSON data
-        const data = await res.json();
-
-        // Return the fetched data
-        return data;
-        
-
-    } catch (error) {
-        console.error("There was a problem with the fetch operation:", error);
-        // Optionally, return an error response or throw the error
-        throw redirect('/login'); // or return { error: error.message };
-        
-
-    }
-};
 
 
 //
