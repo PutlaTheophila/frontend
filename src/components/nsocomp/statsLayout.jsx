6e10b05e8fa7641dc0e1392d0b0c873ea4b19293
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-10-05 22:19:32 +0530

    hello

--- src/components/nsocomp/statsLayout.jsx
+++ src/components/nsocomp/statsLayout.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link , Outlet} from 'react-router-dom'
 import { Users, UserCircle, X, ChevronRight } from 'lucide-react'
 
 const SportSelector = () => {
