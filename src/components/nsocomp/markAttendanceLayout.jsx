7cc6edd831995373062549d281ed1879acaecf09
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-10-05 14:20:18 +0530

    hello:

--- src/components/nsocomp/markAttendanceLayout.jsx
+++ src/components/nsocomp/markAttendanceLayout.jsx
@@ -8,22 +8,18 @@ export default function MarkAttendanceLayout () {
     const user = useLoaderData()
     return(
         <>
-            {
-                user.status === 'success' &&
-                (
-                    <>
-                        <div className="flex flex-col items-center min-h-screen bg-gray-100">
-                            <div className="w-[95vw] md:w-[50vw] max-w-4xl">
-                            <MarkAttendanceNavbar/>
-                            <div className="mt-4">
-                                <Outlet/>
-                            </div>
-                            </div>
-                        </div> 
-                    </>
-                )
-                    
-            }            
+
+            <>
+                <div className="flex flex-col items-center min-h-screen bg-gray-100">
+                    <div className="w-[95vw] md:w-[50vw] max-w-4xl">
+                    <MarkAttendanceNavbar/>
+                    <div className="mt-4">
+                        <Outlet/>
+                    </div>
+                    </div>
+                </div> 
+            </>
+         
             
         </>
     )
