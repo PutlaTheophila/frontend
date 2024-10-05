83fa507750883fabe066141c2ef84e21c3601332
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-10-05 15:43:56 +0530

    hello

--- src/components/nsocomp/markAttendanceLayout.jsx
+++ src/components/nsocomp/markAttendanceLayout.jsx
@@ -2,10 +2,19 @@ import { Outlet, useLoaderData } from "react-router-dom";
 
 import MarkAttendanceNavbar from "./markAttendanceNavbar";
 
+export async function loader() {
+    const res = await fetch("https://terabyte-kvey.onrender.com/api/v1/attendance/interiit/mark-attendance-navbar", {
+        method: 'GET',
+        credentials: 'include'
+    });
+    const data = await res.json();
+    return data;
+}
 
 
 export default function MarkAttendanceLayout () {
     const user = useLoaderData()
+    console.log(user);
     return(
         <>
 
