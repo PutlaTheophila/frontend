1fec647227f0cf04d1492bd7130f75457338c8b3
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-10-07 00:52:53 +0530

    hello

--- src/components/nsocomp/personalnteriitAttendance.jsx
+++ src/components/nsocomp/personalnteriitAttendance.jsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 
 export async function loader({ params }) {
   const sport = params?.sport;
+  console.log('sport from params', sport);
   const res = await fetch(`https://terabyte-kvey.onrender.com/api/v1/attendance/interiit/${sport}`, {
     method: 'GET',
     credentials: 'include'
