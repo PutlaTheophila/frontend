0e17b100cf50543caf23ab8611ac63ed37fd2e2d
putla theophila <putla_theophila@putlas-MacBook-Air.local>
2024-10-11 20:54:39 +0530

    hello

--- src/components/homepagecomp/homeImage.jsx
+++ src/components/homepagecomp/homeImage.jsx
@@ -1,7 +1,7 @@
 function HomeImage() {
     return(
         <>
-            <div class='bg-[url("https://www.iitbhilai.ac.in/index.php?pid=img_dedication_IITBhilai4")] w-[100vw] md:h-[90vh] h-[40vh] md:bg-center bg-center bg-no-repeat bg-cover'>
+            <div class='bg-[url("https://polaris.iitbhilai.ac.in/public/images/nsonss/nso1.jpg")] w-[100vw] md:h-[90vh] h-[40vh] md:bg-center bg-center bg-no-repeat bg-cover'>
                 <div class='bg-gradient-to-t from-slate-950 w-[100vw] md:h-[90vh] h-[40vh] text-white font-titlefont flex items-end font-semibold pb-[20px] text-[25px] sm:text-[40px] pl-[7px]'>
                     Sports | Definition, History, Examples, & Facts | Britannica
                 </div> 
